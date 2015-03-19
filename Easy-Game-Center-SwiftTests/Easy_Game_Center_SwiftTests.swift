29884818edd72bbdd2e7cb516f30130b2ad245c2
DaRkD0G <stephan.yannick@me.com>
2015-03-19 23:55:22 +0100

    Initial Commit

---
+++ Easy-Game-Center-SwiftTests/Easy_Game_Center_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Easy_Game_Center_SwiftTests.swift
+//  Easy-Game-Center-SwiftTests
+//
+//  Created by DaRk-_-D0G on 19/03/2558 E.B..
+//  Copyright (c) 2558 ère b. DaRk-_-D0G. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Easy_Game_Center_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
